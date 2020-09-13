9b3da8dc196b32f9fd6b479910ed1237cf15e369
Sourav Dey <sourav.dey@hashedin.com>
2020-09-13 20:17:43 +0530

    Fixed minor alignment issues in UserAuthentication container

--- src/containers/userAuthentication/index.tsx
+++ src/containers/userAuthentication/index.tsx
@@ -15,22 +15,100 @@ interface UserAuthenticationProps {}
 
 const UserAuthentication: React.FC<UserAuthenticationProps> = props => {
   const [view, setView] = useState<'login' | 'signup'>('login');
+  const [form] = Form.useForm();
 
   const loginOptions = [
     { label: 'Login', value: 'login' },
     { label: 'Signup', value: 'signup' },
   ];
 
+  const onFormFillFinishHandler = (data: any) => {
+    console.log('Received values of form: ', data);
+  };
+
+  const viewChangeHandler = (event: RadioChangeEvent) => {
+    setView(event.target.value);
+  };
+
+  const renderLoginView = () => (
+    <>
+      <Form
+        name='normal_login'
+        className='login-form'
+        initialValues={{ remember: true }}
+        onFinish={onFormFillFinishHandler}>
+        <Form.Item
+          name='loginUsername'
+          rules={[{ required: true, message: 'Please input your Username!' }]}>
+          <Input
+            prefix={<UserOutlined style={{ color: 'gray' }} />}
+            placeholder='Username'
+          />
+        </Form.Item>
+        <Form.Item
+          name='loginPassword'
+          rules={[{ required: true, message: 'Please input your Password!' }]}>
+          <Input.Password
+            prefix={<LockOutlined style={{ color: 'gray' }} />}
+            placeholder='Password'
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name='remember' valuePropName='checked' noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a style={{ float: 'right' }} href=''>
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Button
+              type='primary'
+              htmlType='submit'
+              className='login-form-button'>
+              Log in
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+      <p
+        style={{
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        Don't have an account?
+        <Button
+          style={{ marginLeft: -10 }}
+          type='link'
+          onClick={() => setView('signup')}>
+          Sign up
+        </Button>
+      </p>
+    </>
+  );
+
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 8 },
+      sm: { span: 10 },
     },
     wrapperCol: {
       xs: { span: 24 },
       sm: { span: 16 },
     },
   };
+
   const tailFormItemLayout = {
     wrapperCol: {
       xs: {
@@ -44,189 +122,125 @@ const UserAuthentication: React.FC<UserAuthenticationProps> = props => {
     },
   };
 
-  const [form] = Form.useForm();
-
-  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
-
-  const onFormFillFinishHandler = (data: any) => {
-    console.log('Received values of form: ', data);
-  };
-
-  const viewChangeHandler = (event: RadioChangeEvent) => {
-    setView(event.target.value);
-  };
-
-  const renderLoginView = () => (
-    <Form
-      name='normal_login'
-      className='login-form'
-      initialValues={{ remember: true }}
-      onFinish={onFormFillFinishHandler}>
-      <Form.Item
-        name='username'
-        rules={[{ required: true, message: 'Please input your Username!' }]}>
-        <Input
-          prefix={<UserOutlined className='site-form-item-icon' />}
-          placeholder='Username'
-        />
-      </Form.Item>
-      <Form.Item
-        name='password'
-        rules={[{ required: true, message: 'Please input your Password!' }]}>
-        <Input.Password
-          prefix={<LockOutlined className='site-form-item-icon' />}
-          placeholder='Password'
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name='remember' valuePropName='checked' noStyle>
-          <Checkbox>Remember me</Checkbox>
+  const renderSignupView = () => (
+    <>
+      <Form
+        {...formItemLayout}
+        form={form}
+        name='register'
+        onFinish={onFormFillFinishHandler}
+        scrollToFirstError>
+        <Form.Item
+          name='email'
+          label='E-mail'
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}>
+          <Input />
         </Form.Item>
 
-        <a className='login-form-forgot' href=''>
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Button
-            type='primary'
-            htmlType='submit'
-            className='login-form-button'>
-            Log in
-          </Button>
-          <p>
-            Don't have an account?
-            <Button
-              style={{ marginLeft: -10 }}
-              type='link'
-              onClick={() => setView('signup')}>
-              Sign up
-            </Button>
-          </p>
-        </div>
-      </Form.Item>
-    </Form>
-  );
-
-  const renderSignupView = () => (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name='register'
-      onFinish={onFormFillFinishHandler}
-      scrollToFirstError>
-      <Form.Item
-        name='email'
-        label='E-mail'
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}>
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name='password'
-        label='Password'
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback>
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name='confirm'
-        label='Confirm Password'
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                'The two passwords that you entered do not match!'
-              );
+        <Form.Item
+          name='password'
+          label='Password'
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
             },
-          }),
-        ]}>
-        <Input.Password />
-      </Form.Item>
+          ]}
+          hasFeedback>
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item
-        name='nickname'
-        label={
-          <span>
-            Nickname&nbsp;
-            <Tooltip title='What do you want others to call you?'>
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        }
-        rules={[
-          {
-            required: true,
-            message: 'Please input your nickname!',
-            whitespace: true,
-          },
-        ]}>
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name='confirm'
+          label='Confirm Password'
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  'The two passwords that you entered do not match!'
+                );
+              },
+            }),
+          ]}>
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item
-        name='agreement'
-        valuePropName='checked'
-        rules={[
-          {
-            validator: (_, value) =>
-              value
-                ? Promise.resolve()
-                : Promise.reject('Should accept agreement'),
-          },
-        ]}
-        {...tailFormItemLayout}>
-        <Checkbox>
-          I have read the <a href=''>agreement</a>
-        </Checkbox>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type='primary' htmlType='submit'>
-          Sign up
-        </Button>
-        <p>
-          Already have an account?
-          <Button
-            style={{ marginLeft: -10 }}
-            type='link'
-            onClick={() => setView('login')}>
-            Login
+        <Form.Item
+          name='username'
+          label={
+            <span>
+              User Name&nbsp;
+              <Tooltip title='What do you want others to call you?'>
+                <QuestionCircleOutlined />
+              </Tooltip>
+            </span>
+          }
+          rules={[
+            {
+              required: true,
+              message: 'Please input your nickname!',
+              whitespace: true,
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name='agreement'
+          valuePropName='checked'
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject('Should accept agreement'),
+            },
+          ]}
+          {...tailFormItemLayout}>
+          <Checkbox>
+            I have read the <a href=''>agreement</a>
+          </Checkbox>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type='primary' htmlType='submit'>
+            Sign up
           </Button>
-        </p>
-      </Form.Item>
-    </Form>
+        </Form.Item>
+      </Form>
+      <p
+        style={{
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        Already have an account?
+        <Button
+          style={{ marginLeft: -10 }}
+          type='link'
+          onClick={() => setView('login')}>
+          Login
+        </Button>
+      </p>
+    </>
   );
 
   const renderFormSwitcher = () => (
